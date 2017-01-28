3e4d317e8baf147c7378427ea91d45c1ef6377f9
liuyanhao <374659635@qq.com>
2017-01-28 17:19:26 +0800

    二分法

--- bsearch/bsearch.js
+++ bsearch/bsearch.js
@@ -6,18 +6,61 @@ var bsearchRec = function(arr, low, high, target) {
         throw new Error('not find ' + target);
     }
     var mid = Math.floor((low + high) / 2);
-    if(arr[mid]>target){
-    	return bsearchRec(arr,low,mid,target);
-    }else if(arr[mid]<target){
-    	return bsearchRec(arr,mid,high,target);
-    }else{
-    	return mid;
+    if (arr[mid] > target) {
+        return argument.callee(arr, low, mid, target);
+    } else if (arr[mid] < target) {
+        return argument.callee(arr, mid, high, target);
+    } else {
+        return mid;
     }
 }
 
-try{
-	var array = [1,2,4,5,6];
-	console.log(bsearchRec(array,0,5,4));
-}catch(e){
-	console.log(e);
+try {
+    var array = [1, 2, 4, 5, 6];
+    console.log(bsearchRec(array, 0, 5, 4));
+} catch (e) {
+    console.log(e);
+}
+
+
+//使用循环实现(因为递归是尾递归)
+let bsearchCir = (array, low, high, target) => {
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        if (array[mid] > target) {
+            high = mid - 1;
+        } else if (array[mid] < target) {
+            low = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+}
+
+try {
+    var array = [1, 3, 2, 5, 4, 0];
+    console.log(bsearchCir(array.sort((a, b) => a - b), 0, 5, 4));
+} catch (e) {
+    console.log(e);
+}
+
+
+//二分法找上界
+let bsearchUpperBound = (array, low, high, target) => {
+    if (low > high || target >= array[high]) return -1;
+    let mid = (low + high) / 2;
+    while (low < high) {
+        if (array[mid] > target) {
+            high = mid;
+        }else{
+            mid++;
+        }
+        mid = (low+high)/2;
+    }
+}
+
+var set = new Set([2,3,4,5,6])
+for([key,value] of set.entries()){
+    console.log(key)
 }
