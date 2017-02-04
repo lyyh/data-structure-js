ce6516c8cd96903a80eb6090c56814ad0d875fb6
liuyanhao <374659635@qq.com>
2017-02-04 16:33:52 +0800

    递归中序遍历完全二叉树

--- binarytree/inorder.js
+++ binarytree/inorder.js
@@ -32,3 +32,37 @@ for (var node of inorder(tree)) {
     array.push(node);
 }
 console.log(array)
+
+/**
+ * 使用递归遍历 中序遍历完全二叉树
+ */
+function Tree(left, label, right) {
+    this.left = left;
+    this.label = label;
+    this.right = right;
+}
+
+function inorder(t) {
+    if (t) {
+        inorder(t.left);
+        resultArray.push(t.label);
+        inorder(t.right);
+    }
+}
+
+function make(array) {
+    if (array.length == 1) return new Tree(null, array[0], null);
+    else return new Tree(make(array[0]), array[1], make(array[2]));
+}
+
+var tree = make([
+    [
+        ['a'], 'b', ['c']
+    ], 'd', [
+        ['e'], 'f', ['g']
+    ]
+]);
+
+var resultArray = [];
+inorder(tree);
+console.log(resultArray)
